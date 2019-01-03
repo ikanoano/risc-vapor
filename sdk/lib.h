0ba98b29d9016059e8bdad6bc3bc3c083790b995
ikanoano <ikanoano@users.noreply.github.com>
2019-01-04 04:40:51 +0900

    add mem* functs, fixed link to libgcc

---
+++ sdk/lib.h
@@ -0,0 +1,8 @@
+#define DIGIT_DEC (256)
+#define DIGIT_HEX (8)
+
+void mylib_wait_tx();
+void mylib_display_hex(int);
+void mylib_display_char(int);
+void mylib_display_newline();
+void mylib_finalize();
--- sdk/startup.s
+++ sdk/startup.s
@@ -36,7 +36,7 @@ _start:
     li    x30,  0
     li    x31,  0
     li    sp,   0x07fffff0  # 128MB = 0x08000000
-    j     init_bss          # jump to the main
+    j     _init             # jump to the startup c routine
     nop
     lui   a5,   0xf0000     # halt
     sw    zero, 0(a5)
